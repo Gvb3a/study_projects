4d9522a0c426764c594ec8a1a23e7eb8d249e697
Boris <135196754+Gvb3a@users.noreply.github.com>
2024-05-28 15:35:44 +0300

    Update wolfram.py
    Checking WolframLanguageSession (error window). Fixed select_autocomplete (it inserted text but deleted all other text). Instead of a separate label, a list is made

--- wolfram.py
+++ wolfram.py
@@ -1,23 +1,44 @@
 import customtkinter as ctk
 from wolframclient.evaluation import WolframLanguageSession
 from wolframclient.language import wl, wlexpr
+from datetime import datetime
+from sys import exit
 
-# Start a Wolfram Language session
-session = WolframLanguageSession('D:\\Wolfram\\Mathematica\\MathKernel.exe')
-session.evaluate('1')  # For some reason the first request takes a very long time. (then it is instant)
+
+try:
+    session = WolframLanguageSession()
+    session.evaluate('1')  # For some reason the first request takes a very long time. (then it is instant)
+    print('The session has been successfully connected. Start')
+
+except:
+    root = ctk.CTk()
+    root.title("Error")
+    root.resizable(False, False)
+    root.geometry('370x150')
+    label = ctk.CTkLabel(root, text='Failed to connect to WolframKernel.\n'
+                                    'Try again or if you don\'t have a default\n'
+                                    'installation path specify the path to WolframKernel\n'
+                                    '(if you have Windows, don\'t forget the .exe)', font=("Arial", 16))
+    label.pack(pady=10, padx=10, fill='x')
+    btn = ctk.CTkButton(root, text='Ok', font=("Arial", 16), height=50, command=root.destroy)
+    btn.pack(pady=10, padx=10, fill='x')
+    root.mainloop()
+    exit()
+
 
 root = ctk.CTk()
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("blue")
 root.title("Wolfram Calculator")
-root.geometry('640x165')
-root.resizable(False, False)  # the user will not be able to change the size of the window
+root.geometry('640x210')
+root.resizable(False, True)  # the user will not be able to change the size of the window
 
 last_response = None  # 2-5+ -> error. To ensure that the user does not get an error during the input process, we save the last answer (2-5)
 
 def on_key_release(event):  # function called each time the input field is updated
     current_text = text_var.get()  # get text
     global last_response
+    start_time = datetime.now()
     try:
         wl_result = session.evaluate(wlexpr(current_text))
         pretty_wl_result = session.evaluate(wl.ToString(wl_result, wl.InputForm))
@@ -25,59 +46,65 @@ def on_key_release(event):  # function called each time the input field is updat
             raise ValueError("Incomplete expression; more input is needed")
         else:
             last_response = pretty_wl_result
-            label.configure(text=pretty_wl_result)
+        print(pretty_wl_result)
     except:
-        label.configure(text=str(last_response))
+        pretty_wl_result = last_response
+
+    labels[0].configure(text=str(last_response))
 
     # for autofill
     index = entry.index("insert")  # we'll only take text up to the cursor
     auto_result = autocomplete(current_text[:index])
-    label_autofill.configure(text=", ".join(auto_result))
+    labels[1].configure(text=", ".join(auto_result))
 
-    if auto_result:  # I'll definitely make a proper window change, but not today...
-        root.geometry('640x165')
-    else:
-        root.geometry('640x125')
+
+    print(f'{current_text} -> {pretty_wl_result}. Suggestions: {auto_result}. Time: {datetime.now()-start_time}')
 
 
 def copy_to_clipboard(event):
     root.clipboard_clear()
-    root.clipboard_append(label.cget("text"))
+    root.clipboard_append(labels[0].cget("text"))
     root.update()
     # root.iconify()
 
 
-def on_close():
+def on_close():  # need to terminate the session and had to tie this to the closing of the window
     session.terminate()
     root.destroy()
 
-def check_uppercase(s):
-    uppercase_letters = [c for c in s if c.isupper()]
+
+def last_uppercase(s):
+    uppercase_letters = [c for c in s if c.isupper()]  # uppercase-only list
     if uppercase_letters:
-        last_uppercase_letter = max(uppercase_letters, key=s.index)
-        return True, last_uppercase_letter
+        last_uppercase_letter = max(uppercase_letters, key=s.index)  # find the last uppercase letter by index
+        return last_uppercase_letter
     else:
-        return False, None
+        return False
 
 
 def autocomplete(text):
     global dictionary
-    has_uppercase, last_uppercase = check_uppercase(text)
-    if has_uppercase:
-        suggestions = [word for word in dictionary if word.startswith(text[text.rfind(last_uppercase):])]
+    uppercase = last_uppercase(text)
+    if uppercase:
+        text = text[text.rfind(uppercase):]
+        suggestions = [word for word in dictionary if word.startswith(text)]
         return suggestions
     else:
         return []
 
 
 def select_autocomplete(event):
-    suggestions = label_autofill.cget("text").split(", ")
+    suggestions = labels[1].cget("text").split(", ")
     if suggestions:
-        entry.delete(0, 'end')
-        entry.insert(0, suggestions[0])
-        label_autofill.configure(text="")
-        entry.icursor(ctk.END)  # Putting the cursor at the end of a line
+        index = entry.index("insert")
+        text = text_var.get()
+        last_uppercase_letter = last_uppercase(text[:index])
+        first_index = text.rindex(last_uppercase_letter)
+        entry.delete(first_index, index)
+        entry.insert(entry.index("insert"), suggestions[0])
+        labels[1].configure(text="")
         return 'break'
+
 
 dictionary = ['AbsoluteTiming', 'Solve', 'Sqrt', 'Factor', 'N', 'NSolve', 'ScientificForm', 'Clear']
 
@@ -88,19 +115,22 @@ entry = ctk.CTkEntry(root, textvariable=text_var, font=("Arial", 16), height=40)
 entry.pack(pady=10, padx=10, fill='x')
 
 
-label = ctk.CTkLabel(root, text='Hello', font=("Arial", 16), height=40, wraplength=600)
-label.pack(pady=10, padx=10, fill='x')
+labels = []
+label_text = ['Write to ask Mathematica', 'This will be the autofill for Mathematica commands']
+for i in range(2):
+    label = ctk.CTkLabel(root, text=label_text[i], font=("Arial", 16), wraplength=600)
+    label.pack(pady=10, padx=10, fill='x')
+    labels.append(label)
 
-
-label_autofill = ctk.CTkLabel(root, text='Autofill', font=("Arial", 16), height=40, wraplength=600)
-label_autofill.pack(pady=10, padx=10, fill='x')
+btn = ctk.CTkButton(root, text='Ask WolframAlpha', font=("Arial", 16), height=35, corner_radius=16)
+btn.pack(pady=10, padx=10, fill='x')
 
 
 entry.bind('<KeyRelease>', on_key_release)  # any button in 'entry'
 entry.bind('<Return>', copy_to_clipboard)
 entry.bind('<Tab>', select_autocomplete)
 
-label.bind('<Button-1>', copy_to_clipboard)
+labels[0].bind('<Button-1>', copy_to_clipboard)
 
 root.protocol("WM_DELETE_WINDOW", on_close)
 
